870f4f87ddce1e19adbf82d0ec6c4aed1bf12aa0
Nicole <rosanik3103@gmail.com>
2023-08-18 23:30:24 -0300

    Escreve o haikai

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Pintou estrelas muro");
+Console.WriteLine("E teve o céu");
+Console.WriteLine("Ao alcance das mãos");
+Console.WriteLine("");
+Console.WriteLine("Helena Kolody");
